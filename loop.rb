40f64c6b417fd1fd933c5a30044ca13c3ebd4027
pandix73 <conan11conan112004@gmail.com>
2016-03-06 01:03:46 +0000

    small updated

--- loop.rb
+++ loop.rb
@@ -1,3 +1,14 @@
 5.times {
 	puts "hello"
 }
+
+friends = ["eddie", "joanne", "john", "mark"]
+
+friends.each do |friend|
+  puts friend
+end
+
+friends.each_with_index do |friend, x|
+  puts "#{x} #{friend}"
+end
+
---
+++ method.rb
@@ -0,0 +1,9 @@
+def say_hello_to(name)
+  puts "hello, #{name}"
+end
+
+say_hello_to("eddie")
+
+# parentheses can be omitted
+say_hello_to "eddie"
+
